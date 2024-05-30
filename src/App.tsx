835ccd3ea1c87c529c0c244052779858652fb479
Lucas-Wilian <lucaswilianr.s@gmail.com>
2024-05-30 19:43:29 -0300

    feat: add routes api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useReducer } from 'react';
+
+
+
+const api = axios.create({
+  baseURL:'https://api.mercadopago.com'
+})
+
+
+api.interceptors.request.use(
+  async (config: any): Promise<any> => {
+    const token = process.env.REACT_APP_TOKEN_MERCADO_PAGO_PUBLIC
+    config.headers.Authorization = `Bearer ${token}` 
+
+    return config
+})
+
+
+
+const formReduce = (state:any, event:any) => {
+  return{
+    ...state,
+    [event.name]: event.value
+  }
+}
 
 function App() {
+
+  const [formData, setFormData] = useReducer(formReduce,{})
+
+
+  const handleChange = (event:any) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value
+    })
+  }
+
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+
+    const body = {
+      "transaction_amount": 10,
+      "description":"produto",
+      "payment_method_id":"pix",
+      "payer":{
+        "email":"lucaswilianr.s@gmail.com",
+        "first_name":"",
+        "last_name":"",
+        "identification":{
+          "type":"cpf",
+          "number":"61526577330"
+        },
+        "notification_url":"https://httpdump.io/v5d2z"
+      }
+    }
+
+    console.log(formData)
+
+    api.post("v1/payments", body).then(response =>{
+
+    }).catch(err => {
+      alert(err)
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        API PIX Mercado Pago
+
+        <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <input onChange={handleChange} name='email'/>
+        </div>
+        <div>
+          <label>Nome</label>
+          <input onChange={handleChange} name='nome'/>
+        </div>
+        <div>
+          <label>Cpf</label>
+          <input onChange={handleChange} name='cpf'/>
+        </div>
+        <div>
+          <button type='submit'>Pagar</button>
+        </div>
+        
+      </form>
       </header>
+      
     </div>
   );
 }
